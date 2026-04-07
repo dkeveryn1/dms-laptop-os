d7c93c5a106e07db48b6d4ae4c94b97f3b6b26f0
dkeveryn1 <dkeveryn@protonmail.com>
2026-04-07 16:16:20 -0500

    Create permissions.sh

---
+++ files/scripts/permissions.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+chmod +x /usr/etc/skel/.config/hypr/scripts/quickshell/network/bluetooth_panel_logic.sh
+chmod +x /usr/etc/skel/.config/hypr/scripts/quickshell/network/wifi_panel_logic.sh
