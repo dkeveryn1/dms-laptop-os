f4cc8dbed53ada2ab87c59e78b79add7755deb48
dkeveryn1 <dkeveryn@protonmail.com>
2026-04-08 18:03:11 -0500

    Update permissions.sh

--- files/scripts/permissions.sh
+++ files/scripts/permissions.sh
@@ -2,3 +2,10 @@
 
 chmod +x /usr/etc/skel/.config/hypr/scripts/quickshell/network/bluetooth_panel_logic.sh
 chmod +x /usr/etc/skel/.config/hypr/scripts/quickshell/network/wifi_panel_logic.sh
+
+# --- Fix Nemo's Name and Icon ---
+# Replace 'Name=Files' with 'Name=Nemo'
+sed -i 's/^Name=Files/Name=Nemo/' /usr/share/applications/nemo.desktop
+
+# Replace generic folder icon with the specific Nemo icon
+sed -i 's/^Icon=system-file-manager/Icon=nemo/' /usr/share/applications/nemo.desktop
